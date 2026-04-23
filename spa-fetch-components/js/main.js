5165811a500f039695ae5482fea1053af7ee67dd
migueldinois <miguel.dinois@gmil.com>
2026-04-23 11:13:19 -0300

    feat(logic, components): melhorar a lógica de carregamento de componentes e inicialização de links

--- spa-fetch-components/js/main.js
+++ spa-fetch-components/js/main.js
@@ -4,6 +4,11 @@ const carregarComponente = async (componente, container) => {
     try {
         // await para esperar a resposta do fetch, e depois converte a resposta para texto que é o html do componente
         const resposta = await fetch(`./components/${componente}.html`);
+
+        if (!resposta.ok) {
+            throw new Error(`Erro ao carregar componente ${componente} status: ${resposta.status}`);
+        }
+
         // pega o html do componente 
         const htmlDoComponente = await resposta.text();
         // e insere no container
@@ -14,6 +19,29 @@ const carregarComponente = async (componente, container) => {
     }
 }
 
-carregarComponente('menu', document.querySelector('[data-componente="menu"]')).then(() => {
-    iniciarMenu();
-});
+const iniciarLinks = () => {
+    const conteudo = document.querySelector('[data-conteudo]')
+    let links = document.querySelectorAll('[data-link]');
+
+    links.forEach(link => {
+        //  se eu usei uma async aq em cima ele só vai carregar o componente depois que o fetch tiver terminado, 
+        // e só depois disso ele vai adicionar o evento de click, ou seja, o evento de click só vai ser adicionado 
+        // depois que o componente tiver sido carregado, entao vou usar o await para esperar o carregamento do 
+        // componente antes de adicionar o evento de click
+
+        link.addEventListener('click', async (event) => {
+            event.preventDefault()
+            // dataset é uma forma de acessar os atributos personalizados do html, nesse caso o data-link
+            const pagina = link.dataset.link;
+            const menu = document.querySelector('[data-menu]')
+            const botao = document.querySelector('.menu__botao')
+            if (menu.classList.contains('active')) {
+                menu.classList.remove('active')
+                botao.classList.remove('aria-expanded', false)
+                botao.setAttribute('aria-expanded', 'Abrir menu')
+
+            }
+            await carregarComponente(`./pages/${pagina}.html`, conteudo)
+        })
+    });
+}
