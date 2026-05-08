85f90830d3d805fbf0b5c0bbc62ce54f12aa7d4a
migueldinois <miguel.dinois@aluno.senai.br>
2026-05-08 08:56:46 -0300

    feat(alerts): Feito alertas que estavam faltando

--- sweet-alert/sweet.js
+++ sweet-alert/sweet.js
@@ -33,7 +33,76 @@ botaoErro.addEventListener('click', () => {
     Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: "Something went wrong!",
-        footer: "<a href=\"#\">Porque isso aconteceu</a>"
+        text: "Exemplo de alerta de erro",
+        footer: "<a href=\"#\">Porque isso aconteceu?</a>"
+    });
+})
+
+botaoAviso.addEventListener('click', () => {
+    Swal.fire({
+        title: "Aviso",
+        text: "Exemplo de alerta de Aviso",
+        icon: "warning"
+    });
+})
+
+botaoInformacao.addEventListener('click', () => {
+    Swal.fire({
+        title: "Informação",
+        text: "Exemplo de alerta de Informação",
+        icon: "info"
+    });
+})
+
+botaoConfirmacao.addEventListener('click', () => {
+    Swal.fire({
+        title: "Tem certeza?",
+        text: "Não tem como reverter isso",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim, deletar arquivo",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) Swal.fire({
+            title: "Deleted!",
+            text: "Seu arquivo foi deletado",
+            icon: "success"
+        });
+    });
+})
+
+botaoInput.addEventListener('click', () => {
+    Swal.fire({
+        title: "Escreva seu nome",
+        input: "text",
+        inputAttributes: { autocapitalize: "off" },
+        showCancelButton: true,
+        confirmButtonText: "Confirmar",
+        cancelButtonText: "Cancelar",
+        showLoaderOnConfirm: true,
+    })
+})
+
+botaoTimer.addEventListener('click', () => {
+    let timerInterval;
+    Swal.fire({
+        title: "Alerta de timer",
+        html: "Esse alerta fecha em: <b></b> Milissegundos.",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+                timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) console.log("I was closed by the timer");
     });
 })
